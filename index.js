7287bca811e370ab75523d31449763acf46ed2d1
Perfect Ackah <57051815+perfect-123@users.noreply.github.com>
2026-01-05 23:59:30 -0500

    Branch for unit converter project

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-let basketballProject = document.getElementById("basketball-project")
+let basketballProject = document.getElementById("basketball-project");
 
-basketballProject.addEventListener("click", function(){
-    window.location.href = 'basketball-scoreboard/basketball-index.html'
-})
+basketballProject.addEventListener("click", function () {
+  window.location.href = "basketball-scoreboard/basketball.html";
+});
+
+let unitConverterProject = document.getElementById("unit-converted-project");
+
+unitConverterProject.addEventListener("click", function () {
+  window.location.href = "unit-converter/unit-converter.html";
+});
